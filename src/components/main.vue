ea2a8d324dc017205ecd31b5beb686d0336ef141
sty <1455670697@qq.com>
2021-05-10 22:33:15 +0800

    有点小问题

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,7 +2,7 @@
   <div class="hello" style="height: 100vh">
     <el-container style="height: 100%">
       <el-header style="">
-          <el-dropdown>
+        <el-dropdown>
           <i class="el-icon-setting" style="margin-right: 15px"></i>
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item>查看</el-dropdown-item>
@@ -15,17 +15,18 @@
       <el-container style="height: 92%; border: 0px solid #eee">
         <el-aside width="250px" style="background-color: #3c3f41; min-height: 100%; height: auto;" class="side-bar">
           <div>
-          <el-scrollbar style="height: 100%; ">
-            <el-menu background-color="#3c3f41" active-text-color="#5fc0f7" :default-openeds="['1','2']" text-color="white">
-              <el-submenu index="1">
-                <template slot="title"><i class="el-icon-message"></i>练习题</template>
-                <el-menu-item-group>
-                  <el-menu-item index="1-1" @click="q1" style="padding-left: 25px;">1.	判断三角形类型</el-menu-item>
-                  <el-menu-item index="1-2" @click="q2" style="padding-left: 25px;">2.	电脑销售系统</el-menu-item>
-                  <el-menu-item index="1-3" @click="q3" style="padding-left: 25px;">3.	电信收费问题系统</el-menu-item>
-                  <el-menu-item index="1-4" @click="q4" style="padding-left: 25px;">4.	万年历问题</el-menu-item>
-                </el-menu-item-group>
-  
+            <el-scrollbar style="height: 100%; ">
+              <el-menu background-color="#3c3f41" active-text-color="#5fc0f7" :default-openeds="['1','2']"
+                       text-color="white">
+                <el-submenu index="1">
+                  <template slot="title"><i class="el-icon-message"></i>练习题</template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1" @click="q1" style="padding-left: 25px;">1. 判断三角形类型</el-menu-item>
+                    <el-menu-item index="1-2" @click="q2" style="padding-left: 25px;">2. 电脑销售系统</el-menu-item>
+                    <el-menu-item index="1-3" @click="q3" style="padding-left: 25px;">3. 电信收费问题系统</el-menu-item>
+                    <el-menu-item index="1-4" @click="q4" style="padding-left: 25px;">4. 万年历问题</el-menu-item>
+                  </el-menu-item-group>
+
 
                 </el-submenu>
 
@@ -33,13 +34,13 @@
                   <template slot="title"><i class="el-icon-menu"></i>项目</template>
 
                 </el-submenu>
-            </el-menu>
-          </el-scrollbar>
-        </div>
+              </el-menu>
+            </el-scrollbar>
+          </div>
         </el-aside>
-
-        <router-view></router-view>
-
+        <el-scrollbar style="height: 100%;">
+          <router-view></router-view>
+        </el-scrollbar>
       </el-container>
     </el-container>
   </div>
@@ -87,7 +88,7 @@ export default {
   text-align: right;
   width: 100%;
   z-index: 1;
-  font-size: 12px ;
+  font-size: 12px;
   background-color: #0078d5;
   color: white;
   line-height: 60px;
@@ -97,9 +98,13 @@ export default {
   color: #333;
 }
 
-.el-menu-item{
+.el-menu-item {
 
   text-align: left;
+}
+
+.el-scrollbar__wrap {
+  overflow-x: hidden;
 }
 
 .el-submenu {
